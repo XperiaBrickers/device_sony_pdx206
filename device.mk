9bf80a6a0d1fd88e8dbad4a57df665300f2825b8
Badhri Jagan Sridharan <badhri@google.com>
2022-01-31 22:15:15 -0800

    pdx206: Migrate IUsb implementation to AIDL
    This change migrates IUsb implementation to AIDL.
    Also, IUsb and IUsbGadget now run in its own processes
    to improve stability and isolation.
    
    Port of 3c97a6bb68f6418ee41026f3bd0024405c97cf0c
    
    Bug: 200993386
    Change-Id: I681c1ba9c4f547e3cf5dc0fa7c9ee1aaf4fd603c

--- device.mk
+++ device.mk
@@ -481,7 +481,8 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service.sonykona
+    android.hardware.usb-service.sonykona \
+    android.hardware.usb.gadget-service.sonykona
 
 # Sensors
 PRODUCT_PACKAGES += \
@@ -534,13 +535,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
-
-# USB
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti
-
-PRODUCT_SOONG_NAMESPACES += \
-    vendor/qcom/opensource/usb/etc
 
 # Vendor libstdc++
 PRODUCT_PACKAGES += \
