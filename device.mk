14025f672ca98458e25b3972ba929bb4b4922c59
johnmart19 <johnivan19999@gmail.com>
2022-08-21 20:29:39 +0300

    pdx206: Move Dolby Config to device tree

--- device.mk
+++ device.mk
@@ -287,6 +287,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     device/sony/pdx206/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
 
+# Dolby-Config
+PRODUCT_COPY_FILES += \
+    device/sony/pdx206/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
