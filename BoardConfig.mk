ad2c0198e83dab5acab87d8e7484c7fb22c3a518
SpectreDev-007 <mohammad.galaxy07@gmail.com>
2022-08-25 14:21:49 +0100

    pdx206: fix Vendor BoardConfig name

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -257,4 +257,4 @@ WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 
--include vendor/sony/pdx206/BoardConfig.mk
+-include vendor/sony/pdx206/BoardConfigVendor.mk
