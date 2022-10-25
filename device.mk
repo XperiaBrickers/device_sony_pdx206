1e10912ced5d29c08b157321fb1a4ff0ccd1565b
Luofan Chen <clfbbn@gmail.com>
2022-10-25 19:32:01 +0800

    pdx206: Build Creator Mode setting and related sepolicies
    Build device settings app and address sepolicy denials. Which will allow
    system_app to find and call hal_disp_default.
    
    Change-Id: Icb4b0cb1086e75734602008751221639c55a06ec

--- device.mk
+++ device.mk
@@ -561,6 +561,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-wfd.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-wfd.xml
 
+# Creator Mode
+PRODUCT_PACKAGES += \
+    CreatorMode
+
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
