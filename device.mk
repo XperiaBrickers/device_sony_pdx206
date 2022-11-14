b8f02251f91f5fdbd66c54964072711783fce121
Albert I <kras@raphielgang.org>
2022-11-14 21:08:30 +0000

    pdx206: inherit several Android Go configurations
    Beneficial for everyone, even on non-Go devices.

--- device.mk
+++ device.mk
@@ -352,6 +352,10 @@ PRODUCT_PACKAGES += \
     libipanat \
     liboffloadhal
 
+# Inherit several Android Go Configurations (Beneficial for everyone, even on non-Go devices)
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
